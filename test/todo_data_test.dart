4510f4fee98efc1a44d31bd11c755fa6a2d9c2c2
dramosv90 <dariel.ramos.dv@gmail.com>
2019-11-14 16:06:35 -0500

    refactor unit tests that used wrong testWidgets function

--- test/todo_data_test.dart
+++ test/todo_data_test.dart
@@ -4,14 +4,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:todo_example/classes/list_data.dart';
 
 void main() {
-  testWidgets('Serialize ToDo Data correctly', (WidgetTester tester) async {
+  test('Serialize ToDo Data correctly', () async {
     int createdAt =  DateTime.now().millisecondsSinceEpoch;
     ToDoData data = ToDoData.fromData(name: 'Dariel', checked: true, createdAt: createdAt);
     String encoded = jsonEncode(data);
     expect(encoded, '{"name":"Dariel","checked":true,"created_at":'+createdAt.toString()+'}');
   });
 
-  testWidgets('Deserialize ToDo Data correctly', (WidgetTester tester) async {
+  test('Deserialize ToDo Data correctly', () async {
     int createdAt =  DateTime.now().millisecondsSinceEpoch;
     String serializedData = '{"name":"Dariel","checked":true,"created_at":'+createdAt.toString()+'}';
     ToDoData data = ToDoData.fromJson(jsonDecode(serializedData));
@@ -20,7 +20,7 @@ void main() {
     expect(data.createdAt, createdAt);
   });
 
-  testWidgets('Serialize ToDo Data List correctly', (WidgetTester tester) async {
+  test('Serialize ToDo Data List correctly', () async {
     ToDoDataList list = ToDoDataList();
     list.add(ToDoData.fromData(name: 'Testing', checked: true));
     list.add(ToDoData.fromData(name: 'Testing123', checked: false));
@@ -28,7 +28,7 @@ void main() {
     expect(serialized, '{"list":"[{\\"name\\":\\"Testing\\",\\"checked\\":true,\\"created_at\\":null},{\\"name\\":\\"Testing123\\",\\"checked\\":false,\\"created_at\\":null}]"}');
   });
 
-  testWidgets('Deserialize ToDo Data List correctly', (WidgetTester tester) async {
+  test('Deserialize ToDo Data List correctly', () async {
     final encoded = '{"list":"[{\\"name\\":\\"Testing\\",\\"checked\\":true,\\"created_at\\":null},{\\"name\\":\\"Testing123\\",\\"checked\\":false,\\"created_at\\":null}]"}';
     ToDoDataList list = ToDoDataList.fromJson(jsonDecode(encoded));
     expect(list.count, 2);
