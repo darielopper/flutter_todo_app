5c3ca885e1be1389fef8d10aba3f26c27021a9b3
Dariel Ramos <dariel.ramos.dv@gmail.com>
2019-11-20 15:44:02 -0500

    Add animations to all modals (#8)
    * adding scale animation to add task modal
    
    * add animation to confirm modal
    
    * finish work fixing analize

--- lib/classes/utils.dart
+++ lib/classes/utils.dart
@@ -10,73 +10,95 @@ class Utils
     String title = 'Delete Task',
     String message = 'Are you sure do you want remove this Task?'
   }) async {
-    return showDialog<bool>(
+    return showGeneralDialog<bool>(
       context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: SingleChildScrollView(
-            child: Text(message)
+      barrierColor: Colors.black.withOpacity(0.5),
+      barrierLabel: '',
+      barrierDismissible: true,
+      transitionDuration: Duration(milliseconds: 200),
+      transitionBuilder: (context, anim1, anim2, widget) {
+        return Transform.scale(
+          scale: anim1.value,
+          child: Opacity(
+            opacity: anim1.value,
+            child: AlertDialog(
+              title: Text(title),
+              content: SingleChildScrollView(
+                child: Text(message)
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  key: Key('cancel_btn'),
+                  child: Text('Cancel'),
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  onPressed: () { Navigator.of(context).pop(false); },
+                ),
+                FlatButton(
+                  key: Key('confirm_btn'),
+                  child: Text('Ok'),
+                  onPressed: () { Navigator.of(context).pop(true); },
+                ),
+              ],
+            ),
           ),
-          actions: <Widget>[
-            FlatButton(
-              key: Key('cancel_btn'),
-              child: Text('Cancel'),
-              color: Colors.red,
-              textColor: Colors.white,
-              onPressed: () { Navigator.of(context).pop(false); },
-            ),
-            FlatButton(
-              key: Key('confirm_btn'),
-              child: Text('Ok'),
-              onPressed: () { Navigator.of(context).pop(true); },
-            ),
-          ],
         );
-      }
+      },
+      pageBuilder: (contex, anim1, anim2) => Container()
     );
   }
 
   static Future<String> addEditDialog({String text = '', String title = 'Add New Task', BuildContext context}) async {
     TextEditingController _controller = TextEditingController(text: text);
-    return showDialog<String>(
+    return showGeneralDialog<String>(
       context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title, key: Key('add_dialog_title')),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                TextField(
-                  key: Key('add_dialog_text_field'),
-                  controller: _controller,
-                  decoration: InputDecoration(hintText: 'Description'),
-                  autofocus: true,
-                  onSubmitted: (val) => Navigator.of(context).pop(val),
+      barrierDismissible: true,
+      barrierLabel: '',
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: Duration(milliseconds: 200),
+      transitionBuilder: (context, anim1, anim2, widget) {
+        return Transform.scale(
+          scale: anim1.value,
+          child: Opacity(
+            opacity: anim1.value,
+            child: AlertDialog(
+              title: Text(title, key: Key('add_dialog_title')),
+              content: SingleChildScrollView(
+                child: ListBody(
+                  children: <Widget>[
+                    TextField(
+                      key: Key('add_dialog_text_field'),
+                      controller: _controller,
+                      decoration: InputDecoration(hintText: 'Description'),
+                      autofocus: true,
+                      onSubmitted: (val) => Navigator.of(context).pop(val),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  key: Key('add_dialog_cancel_button'),
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    _controller.clear();
+                  }
+                ),
+                FlatButton(
+                  key: Key('add_dialog_ok_button'),
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop(_controller.text != null ? _controller.text : '');
+                    _controller.clear();
+                  }
                 ),
               ],
-            ),
+            )
           ),
-          actions: <Widget>[
-            FlatButton(
-              key: Key('add_dialog_cancel_button'),
-              child: Text('Cancel'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                _controller.clear();
-              }
-            ),
-            FlatButton(
-              key: Key('add_dialog_ok_button'),
-              child: Text('Ok'),
-              onPressed: () {
-                Navigator.of(context).pop(_controller.text != null ? _controller.text : '');
-                _controller.clear();
-              }
-            ),
-          ],
         );
       },
+      pageBuilder: (context, anim1, anim2) => Container()
     );
   }
 
