1b1e90070fe361b260bcedfbf396c9320b878840
Dariel Ramos <dariel.ramos.dv@gmail.com>
2019-11-21 18:46:49 -0500

    avoid change display (#9)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'package:todo_example/components/app.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  runApp(MyApp());
+}
